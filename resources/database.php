b1307653b85a7e727e7393f0e6f9a502a675f084
David Whalley <77077742+dwhalley15@users.noreply.github.com>
2024-05-03 07:45:26 +0000

    Database taken down

--- resources/database.php
+++ resources/database.php
@@ -1,10 +1,10 @@
 <?php
     //Connects to the database.
     function connect(){
-      $username = "g5416741";
-      $password = "hplj4000";
-      $host = "db.db4free.net";
-      $port = 3306;
+      $username = "your_username_here";
+      $password = "your_password_here";
+      $host = "your_hostname_here";
+      $port = 0000;
       $database = $username;
       $conn = new mysqli();
       $conn->init();
