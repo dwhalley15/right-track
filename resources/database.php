1f5110327d2f58115ac375bb8fc1a32861e23918
David Whalley <77077742+dwhalley15@users.noreply.github.com>
2024-05-05 14:12:51 +0000

    Re attached database with envs

--- resources/database.php
+++ resources/database.php
@@ -1,10 +1,10 @@
 <?php
     //Connects to the database.
     function connect(){
-      $username = "your_username_here";
-      $password = "your_password_here";
-      $host = "your_hostname_here";
-      $port = 0000;
+      $username = getenv("USER_NAME");
+      $password = getenv("PASS");
+      $host = getenv("HOST");
+      $port = getenv("PORT");
       $database = $username;
       $conn = new mysqli();
       $conn->init();
